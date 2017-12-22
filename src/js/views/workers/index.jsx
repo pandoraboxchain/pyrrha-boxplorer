d2fe844955dd27b3e6e9a7f2b51bdbf65adec111
konchunas@gmail.com <konchunas@gmail.com>
2017-12-22 15:37:48 +0200

    websocket update experimental support

--- src/js/views/workers/index.jsx
+++ src/js/views/workers/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Websocket from 'react-websocket';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { actions as Actions } from '../../redux/modules/boxchainModule';
 import { exampleSelector } from '../../redux/selectors/exampleSelector';
 import { WorkersComponent } from '../../common/components/Workers/';
+import { Map } from 'immutable';
 
 const config = require('../../../../config/development.json');
 
@@ -26,15 +28,37 @@ class WorkersView extends Component {
     example: PropTypes.object.isRequired,
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {websocketData: {} };
+  }
+
   componentWillMount() {
     this.props.setUrl(config.node_url+'/store', 'Home');
   }
 
+  handleUpdateEvent(data)
+  {
+    var map = Map({
+      requestRaw: JSON.parse(data)
+    })
+
+    var websocketData = {example: {result: map } };
+    
+    this.setState({ websocketData: websocketData });
+
+  }
+  //<Websocket url={'ws://'+config.node_url+'/workers/listener/'}
+
   render() {
+    const workers = {...this.props, ...this.state.websocketData}
     return (
       <div>
         <h1 className="title">Workers</h1>
-        <WorkersComponent {...this.props} />
+        <WorkersComponent {...workers} />
+
+        <Websocket url='ws://localhost:1337/' debug={true}
+            onMessage={this.handleUpdateEvent.bind(this)}/>
       </div>
     )
   }
