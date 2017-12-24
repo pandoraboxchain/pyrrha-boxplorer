9e51c1bb6fca855771d3b70b2dacf90d105d98f7
konchunas@gmail.com <konchunas@gmail.com>
2017-12-24 16:13:38 +0200

    Handle job jsons merging when type:changed received from websocket

--- src/js/utility/websocketHelper.js
+++ src/js/utility/websocketHelper.js
@@ -2,8 +2,50 @@ import { Map } from 'immutable';
 
 function appendNewFields(json, newJson) {
     for(var key in json) {
-        json[key].push(...newJson[key]);
+        mergeEntries(json[key], newJson[key]);
     }
+}
+
+function mergeEntries(list, newList)
+{
+    var changedEntries = [];
+    var createdEntries = [];
+    for (let i = 0; i < newList.length; i++) {
+        var entry = newList[i]
+        if ("type" in entry) {
+            if (entry["type"] == "changed") {
+                changedEntries.push(entry);
+                continue;
+            }
+        }
+        createdEntries.push(entry);
+    }
+
+    for (let i = 0; i < changedEntries.length; i++) {
+        let entry = changedEntries[i];
+        //TODO: replace with "address" for consistency when ready on backend
+        var changedEntryIndex = findEntryWithField(list, "jobAddress", entry["jobAddress"]);
+        if (changedEntryIndex != -1)
+        {
+            list[changedEntryIndex] = entry;
+        }
+    }
+
+    list.push(...createdEntries);
+}
+
+function findEntryWithField(list, key, value)
+{
+    for (var i = 0; i < list.length; i++)
+    {
+        var entry = list[i];
+        if (key in entry) {
+            if (entry[key] == value) {
+                return i;
+            }
+        }
+    }
+    return -1;
 }
 
 function getRawRequest(container) {
